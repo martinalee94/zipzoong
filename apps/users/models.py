1ac9b735e1249afaea10c3371b40e9798e0e3455
Martina Lee <martinalee0528@gmail.com>
2022-06-30 00:13:52 +0900

    feat: seller, agent 기본 모델 생성

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from apps.commons.models import TimeStampModel
+
+
+class Seller(TimeStampModel, models.Model):
+    id = models.CharField(
+        verbose_name="판매자 일련번호", primary_key=True, max_length=32, db_index=True
+    )
+    uniq_num = models.CharField(verbose_name="판매자 고유넘버", max_length=16, db_index=True)
+    nickname = models.CharField(verbose_name="판매자 닉네임", max_length=20, null=True)
+    phone = models.CharField(verbose_name="판매자 전화번호", max_length=11, null=True)
+
+
+class Agent(TimeStampModel, models.Model):
+    id = models.CharField(verbose_name="중계사 일련번호", primary_key=True, max_length=32)
