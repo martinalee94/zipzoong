fc1ac0f3d95809f3217c7c3e731580ca049af5c1
Martina Lee <martinalee0528@gmail.com>
2022-06-30 22:00:12 +0900

    chore: swagger 추가

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Zipzoong API",
+        default_version="0.1",
+        description="Zipzoong API Docs",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 extrapatterns = [
+    path(
+        "docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path("houses/", include("apps.houses.urls")),
     path("sellers/", include("apps.sellers.urls")),
 ]
