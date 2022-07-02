fcb12be54e94559cd04b421b9deb7aaa5dac7e03
Martina Lee <martinalee0528@gmail.com>
2022-07-02 23:48:22 +0900

    chore: cors 설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,6 +13,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 THIRD_APPS = [
+    "corsheaders",
     "drf_yasg",
     "rest_framework",
 ]
@@ -35,6 +36,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -4,3 +4,5 @@ from .base import *  # noqa: F403
 class DevelopmentConfig:
     DEBUG = True
     ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+    CORS_ORIGIN_WHITELIST = ["http://127.0.0.1:8000", "http://localhost:8000"]
+    CORS_ALLOW_CREDENTIALS = True
