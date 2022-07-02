42b6713712617509c28f4fb92a923325acf7e8b4
Martina Lee <martinalee0528@gmail.com>
2022-07-03 01:03:40 +0900

    feat: House 관련 모델 수정 사항 반영

--- apps/houses/models.py
+++ apps/houses/models.py
@@ -5,18 +5,20 @@ from apps.users.models import Agent, Seller
 
 
 class House(TimeStampModel, models.Model):
-    id = models.CharField(verbose_name="집 일련번호", primary_key=True, max_length=32)
-    type = models.CharField(verbose_name="주거 형태", max_length=4, db_index=True)
-    contract_type = models.CharField(verbose_name="판매 유형", max_length=4, db_index=True)
+    # id = models.CharField(verbose_name="집 일련번호", primary_key=True, max_length=32)
+    type = models.CharField(verbose_name="주거 형태", max_length=4, null=True, db_index=True)
+    contract_type = models.CharField(verbose_name="판매 유형", max_length=4, null=True, db_index=True)  # TODO: null값 허용?
     sell_price = models.IntegerField(verbose_name="매매가", null=True)
     charter_rent_price = models.IntegerField(verbose_name="전세가", null=True)
     monthly_rent_price = models.IntegerField(verbose_name="월세가", null=True)
     deposit_rent_price = models.IntegerField(verbose_name="보증금", null=True)
-    sido_addr = models.CharField(verbose_name="시도", max_length=32)
-    gungu_addr = models.CharField(verbose_name="군구", max_length=32)
-    street_addr = models.CharField(verbose_name="도로명", max_length=128)
-    detail_addr = models.CharField(verbose_name="상세주소", max_length=256)
-    postal_code = models.CharField(verbose_name="우편번호", max_length=6)
+    full_addr = models.CharField(verbose_name="전체 주소", max_length=256)
+    # TODO: 주소 어떻게 자를지 고민 필요함
+    sido_addr = models.CharField(verbose_name="시도", max_length=32, null=True)
+    gungu_addr = models.CharField(verbose_name="군구", max_length=32, null=True)
+    street_addr = models.CharField(verbose_name="도로명", max_length=128, null=True)
+    detail_addr = models.CharField(verbose_name="상세주소", max_length=256, null=True)
+    postal_code = models.CharField(verbose_name="우편번호", max_length=6, null=True)
     seller = models.ForeignKey(
         Seller,
         related_name="house",
@@ -30,19 +32,23 @@ class House(TimeStampModel, models.Model):
 
 
 class HouseOption(TimeStampModel, models.Model):
-    id = models.CharField(verbose_name="집 일련번호", primary_key=True, max_length=32)
+    house = models.ForeignKey(
+        House,
+        related_name="options",
+        on_delete=models.CASCADE,
+    )
     # TODO: 옵션 종류 협의 필요
-    air_conditioner = models.SmallIntegerField(verbose_name="냉장고")
+    aircon = models.SmallIntegerField(verbose_name="냉장고")
 
     class Meta:
         db_table = "house_option"
 
 
 class HouseImage(TimeStampModel, models.Model):
-    house = models.ForeignKey(House, related_name="image", on_delete=models.CASCADE)
+    house = models.ForeignKey(House, related_name="images", on_delete=models.CASCADE)
     path = models.CharField(verbose_name="사진 경로", max_length=256)
     name = models.CharField(verbose_name="사진명", max_length=256)
-    extension = models.CharField(verbose_name="사진 확장자", max_length=256)
+    type = models.CharField(verbose_name="사진 확장자", max_length=256)
     size = models.IntegerField(verbose_name="사진 사이즈")
     width = models.IntegerField(verbose_name="사진 가로 길이")
     height = models.IntegerField(verbose_name="사진 세로 길이")
@@ -53,13 +59,20 @@ class HouseImage(TimeStampModel, models.Model):
 
 
 class HouseBidInfo(TimeStampModel, models.Model):
+    yes_no = [("Y", "Yes"), ("N", "No")]
     house = models.ForeignKey(House, related_name="bid_info", on_delete=models.CASCADE)
     agent = models.ForeignKey(Agent, related_name="bid_info", on_delete=models.CASCADE)
     bid_price = models.IntegerField(verbose_name="중개 수수료 입찰가")
     contact_req = models.CharField(
         verbose_name="연락요청 여부",
         max_length=2,
-        choices=[("Y", "Yes"), ("N", "No")],
+        choices=yes_no,
+        default="N",
+    )
+    success_bid = models.CharField(
+        verbose_name="낙찰 성공 여부",
+        max_length=2,
+        choices=yes_no,
         default="N",
     )
     kind_rating = models.SmallIntegerField(verbose_name="친절도")
@@ -70,5 +83,5 @@ class HouseBidInfo(TimeStampModel, models.Model):
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["house", "agent"], name="unique_bid")
+            models.UniqueConstraint(fields=["house", "agent"], name="unique_bid"),
         ]
