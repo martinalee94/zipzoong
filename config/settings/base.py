425cc1e91bb0fa02bb7a4f6f2928c7aa59b1648c
Martina Lee <martinalee0528@gmail.com>
2022-07-03 19:38:51 +0900

    feat: 도커 파일 초기 설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,8 +76,8 @@ DATABASES = {
         "NAME": os.getenv("DEV_DB_NAME"),
         "USER": os.getenv("DEV_DB_USER"),
         "PASSWORD": os.getenv("DEV_DB_PASSWORD"),
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
+        "HOST": os.getenv("DEV_DB_HOST"),
+        "PORT": os.getenv("DEV_DB_PORT"),
     }
 }
 
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
