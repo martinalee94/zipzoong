e57db119c4f58ec9fc2d5442476cc0fc9dbcf7f5
Martina Lee <martinalee0528@gmail.com>
2022-07-10 19:16:54 +0900

    chore: 프로젝트 세팅

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,5 +1,8 @@
-from .base import *
+from .base import *  # noqa: F403
 
-class DevelopmentConfig():
+
+class DevelopmentConfig:
     DEBUG = True
-    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+    ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+    CORS_ORIGIN_WHITELIST = ["http://127.0.0.1:8000", "http://localhost:8000"]
+    CORS_ALLOW_CREDENTIALS = True
