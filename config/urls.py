54eca52aac5a4c6207d0d450d865a36e4df37e94
Martina Lee <martinalee0528@gmail.com>
2022-07-10 19:30:12 +0900

    chore: timestamp 모델 및 응답 renderer 추가, url기본 구조생성

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Zipzoong API",
+        default_version="0.1",
+        description="Zipzoong API Docs",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+extrapatterns = [
+    path(
+        "docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("houses/", include("apps.houses.urls")),
+    path("users/", include("apps.users.urls")),
+]
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(extrapatterns)),
 ]
