81ed1f1f619d7513069fd7e01f2db02794970d0c
Martina Lee <martinalee0528@gmail.com>
2022-08-07 20:40:48 +0900

    chore: 장고관련 삭제

--- config/settings/dev.py
+++
@@ -1,5 +0,0 @@
-from .base import *
-
-class DevelopmentConfig():
-    DEBUG = True
-    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
--- config/settings/prod.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-
-class ProductionConfig():
-    DEBUG = False
-    ALLOWED_HOSTS = ['localhost'] #TODO: 추후 서버 주소 추가 필요
-
