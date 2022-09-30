3f81cb7aea59803235d40b16e90a92406f22289c
Martina Lee <martinalee0528@gmail.com>
2022-09-30 23:55:44 +0900

    change to django-ninja

--- apps/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django import views
-from django.urls import include, path
-
-from .views import SellerHouseInfoView, SellerRegisterView
-
-urlpatterns = [
-    path("sellers/register", SellerRegisterView.as_view(), name="seller_register"),
-    path("sellers/house-info", SellerHouseInfoView.as_view(), name="seller_house_info"),
-]
