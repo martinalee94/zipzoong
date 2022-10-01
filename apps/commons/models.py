5549525821d6cf3720c08b3cc1a67edf93d96768
Martina Lee <martinalee0528@gmail.com>
2022-10-02 03:16:00 +0900

    commons added

--- apps/commons/models.py
+++ apps/commons/models.py
@@ -3,10 +3,8 @@ from django.utils import timezone
 
 
 class TimeStampModel(models.Model):
-    created_at = models.DateTimeField(
-        verbose_name="생성 날짜", db_index=True, default=timezone.now
-    )
-    modified_at = models.DateTimeField(verbose_name="수정 날짜", auto_now=True)
+    created_dt = models.DateTimeField(verbose_name="생성 날짜", db_index=True, default=timezone.now)
+    modified_dt = models.DateTimeField(verbose_name="수정 날짜", auto_now=True)
 
     class Meta:
         abstract = True
